0bdd44df05eb756a748fd04b7e28dfec9d8b1545
twoplustwofive <rathod18@iitg.ac.in>
2020-02-01 15:09:00 +0530

    Main base created

--- Diary_Portal/diary/models.py
+++ Diary_Portal/diary/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class company(models.Model):
+    CompanyName = models.CharField(max_length=100)
+    POC         = models.CharField(max_length=100)
+    CPOC        = models.CharField(max_length=100)
+    Remarks     = models.TextField()
+    order       = models.AutoField(primary_key=True)
--- Diary_Portal/diary/views.py
+++ Diary_Portal/diary/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import company
 # Create your views here.
+def gethomepage(request):
+    list = company.objects.all()
+    return render(request,'diary/base.html',{'company':list})
