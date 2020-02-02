81d8295ebc2e90ec9b1348bd118607d096386714
twoplustwofive <rathod18@iitg.ac.in>
2020-02-02 21:25:47 +0530

    Search bar added

--- Diary_Portal/diary/models.py
+++ Diary_Portal/diary/models.py
@@ -6,6 +6,7 @@ class company(models.Model):
     POC         = models.CharField(max_length=100)
     CPOC        = models.CharField(max_length=100)
     order       = models.AutoField(primary_key=True)
+
     def __str__(self):
         return self.CompanyName
 
